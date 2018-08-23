9efe793fd6f9c4b5b035ee08747e54b894fbb352
Jesus bossa <jesusbossa@protonmail.com>
2018-08-22 20:07:07 -0500

    write db improved

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const app = express()
 const socket = require('socket.io')
+const fs = require('fs')
+let {info} = require('./db.json')
 
 app.set('port', process.env.PORT || 3000)
 
-// app.get('/', (req, res) => res.send('<h1>Hola socket</h1>'))
+app.get('/', (req, res) => res.send(info))
 
 const io = socket.listen(app.listen(app.get('port'), () => console.log('Serving on port: ', app.get('port'))))
 
 io.on('connection', (socket) => {
     console.log(`Nueva conexion: ${socket.id}`)
+    io.sockets.emit('push:connection', info)
 
-    socket.on('conf:message', (data) => {
-        io.sockets.emit('conf:message', data)
-        console.log(data)
-        io.sockets.emit('conf:time', 'domingo')
+    socket.on('push:message', (data) => {
+        info.push(data)
+        io.sockets.emit('push:message', info[info.length -1])
+        // let temp = JSON.stringify(info)
+        // fs.writeFileSync(__dirname +'/db.json', temp, (err) => {
+        //     if (err) throw err
+        //     console.log('Writed')
+        // })
     })
 })
