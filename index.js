083e28752fe324b13eb8572eaffe49395ca3cb0c
Jesus bossa <jesusbossa@protonmail.com>
2018-08-23 21:56:29 -0500

    headers implemented

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const fs = require('fs')
 let {info} = require('./db.json')
 let {users} = require('./db.json')
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
 app.set('port', process.env.PORT || 3000)
 
 app.get('/', (req, res) => res.send(info))
